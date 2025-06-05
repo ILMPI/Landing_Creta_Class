824ed364e2ddbcd0d519b487715f1e25bdc09bec
Angie <eguzkia6@gmail.com>
2025-06-05 06:17:59 +0200

    added swiper + 3 cards content

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,42 @@
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
+const swiper = new Swiper('.swiper', {
+	// slidesPerView: 3,
+	loop: true,
+	spaceBetween: 42,
+	// slidesPerGroup: 3,
+	speed: 600,
+	navigation: {
+		nextEl: '.learningSliderNext',
+		prevEl: '.learningSliderPrev',
+	},
+	pagination: {
+		el: '.learningSliderPag',
+		clickable: true,
+		renderBullet: function (index, className) {
+			return `<span class="bullet ${className}"></span>`;
+		},
+	},
+	breakpoints: {
+		1024: {
+			slidesPerView: 3,
+			slidesPerGroup: 3,
+			pagination: {
+				type: 'bullets',
+			},
+		},
+		768: {
+			slidesPerView: 2,
+			slidesPerGroup: 2,
+			pagination: {
+				type: 'bullets',
+			},
+		},
+		0: {
+			slidesPerView: 1,
+			slidesPerGroup: 1,
+			pagination: {
+				type: 'fraction',
+			},
+		},
+	},
+});
