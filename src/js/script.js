a1450ab927ff436436058c8a94459dbc55a7fa62
Angie <eguzkia6@gmail.com>
2025-06-09 05:55:11 +0200

    added tabs

--- src/js/script.js
+++ src/js/script.js
@@ -40,3 +40,100 @@ const swiper = new Swiper('.swiper', {
 		},
 	},
 });
+
+//tabs
+const tabsContainer = document.querySelector('.tabs__container');
+const tabsList = tabsContainer.querySelector('.tabs__title');
+const tabButtons = tabsList.querySelectorAll('a');
+const tabPanels = tabsContainer.querySelectorAll('.tabs__panels > div');
+
+tabsList.setAttribute('role', 'tablist');
+
+tabsList.querySelectorAll('li').forEach(listItem => {
+	listItem.setAttribute('role', 'presentation');
+});
+tabButtons.forEach((tab, index) => {
+	tab.setAttribute('role', 'tab');
+	tab.setAttribute('id', `tab_${index + 1}`);
+	tab.setAttribute('aria-controls', `S${index + 1}`);
+
+	tabPanels[index].setAttribute('role', 'tabpanel');
+	tabPanels[index].setAttribute('aria-labelledby', `tab_${index + 1}`);
+
+	if (index === 1) {
+		tab.setAttribute('aria-selected', 'true');
+		tab.removeAttribute('tabindex');
+		tabPanels[index].removeAttribute('hidden');
+	} else {
+		tab.setAttribute('aria-selected', 'false');
+		tab.setAttribute('tabindex', '-1');
+		tabPanels[index].setAttribute('hidden', '');
+	}
+});
+
+tabsContainer.addEventListener('click', e => {
+	const clickedTab = e.target.closest('a');
+	if (!clickedTab) return;
+	e.preventDefault();
+
+	switchTab(clickedTab);
+});
+
+function switchTab(newTab) {
+	const activePanelId = newTab.getAttribute('href');
+	const activePanel = tabsContainer.querySelector(activePanelId);
+
+	tabButtons.forEach(tab => {
+		tab.setAttribute('aria-selected', 'false');
+		tab.setAttribute('tabindex', '-1');
+	});
+
+	tabPanels.forEach(panel => {
+		panel.setAttribute('hidden', '');
+	});
+
+	newTab.setAttribute('aria-selected', 'true');
+	newTab.removeAttribute('tabindex');
+	activePanel.removeAttribute('hidden');
+}
+
+// tabButtons.forEach((tab, index) => {
+// 	tab.setAttribute('role', 'tab');
+// 	if (index === 1) {
+// 		tab.setAttribute('aria-selected', 'true');
+// 	} else {
+// 		tab.setAttribute('tabindex', '-1');
+// 		tabPanels[index].setAttribute('hidden', '');
+// 	}
+// });
+
+// tabPanels.forEach(panel => {
+// 	panel.setAttribute('role', 'tab');
+// 	panel.setAttribute('tabindex', '0');
+// });
+
+// tabsContainer.addEventListener('click', e => {
+// 	const clickedTab = e.target.closest('a');
+// 	if (!clickedTab) return;
+// 	e.preventDefault();
+
+// 	switchTab(clickedTab);
+// });
+
+// function switchTab(newTab) {
+// 	const activePanelId = newTab.getAttribute('href');
+// 	const activePanel = tabsContainer.querySelector(activePanelId);
+
+// 	tabButtons.forEach(button => {
+// 		button.setAttribute('aria-selected', false);
+// 		button.setAttribute('tabindex', '-1');
+// 	});
+
+// 	tabPanels.forEach(panel => {
+// 		panel.setAttribute('hidden', true);
+// 	});
+// 	activePanel.removeAttribute('hidden');
+
+// 	newTab.setAttribute('aria-selected', 'true');
+// 	newTab.setAttribute('tabindex', '0');
+// }
